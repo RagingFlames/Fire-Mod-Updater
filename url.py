6fce9caf14a6a2337aa1dd485058ac22393f4906
RagingFlames <mclemoret97@gmail.com>
2023-12-04 16:18:55 -0500

    Change IP address

--- url.py
+++ url.py
@@ -2,12 +2,12 @@ message = ""
 version = "2.2"
 packs = {
     "Stellaris": {
-        "v3.9": ["http://100.11.30.28:160/Public/Stellaris/3.9/mod.7z", "a85f"],
-        "v3.8": ["http://100.11.30.28:160/Public/Stellaris/3.8/mod.7z", "a85f"],
-        "v3.7": ["http://100.11.30.28:160/Public/Stellaris/3.7/Tyler%20Comp%2037x.7z", "UNKNOWN"],
+        "v3.9": ["http://96.227.58.11:160/Public/Stellaris/3.9/mod.7z", "a85f"],
+        "v3.8": ["http://96.227.58.11:160/Public/Stellaris/3.8/mod.7z", "a85f"],
+        "v3.7": ["http://96.227.58.11:160/Public/Stellaris/3.7/Tyler%20Comp%2037x.7z", "UNKNOWN"],
     },
     "Hearts of Iron IV": {
-        "invoke": ["http://100.11.30.28:160/Public/Hearts%20of%20Iron%20IV/invoke.7z", "lmao"]
+        "invoke": ["http://96.227.58.11:160/Public/Hearts%20of%20Iron%20IV/invoke.7z", "lmao"]
 
     }
 
