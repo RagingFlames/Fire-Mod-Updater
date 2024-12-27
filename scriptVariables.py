6639f094bfd029e8c6417c22ffba80de4a42ac83
unknown <mclemoret97@gmail.com>
2024-12-27 15:54:41 -0500

    Use example.com

--- scriptVariables.py
+++ scriptVariables.py
@@ -2,13 +2,13 @@ message = ""
 version = "2.3"
 packs = {
     "Stellaris": {
-        "invoke": ["http://96.227.58.11:160/Public/Hearts%20of%20Iron%20IV/invoke.7z", "lmao"],
+        "invoke": ["example.com/Public/Hearts%20of%20Iron%20IV/invoke.7z", "lmao"],
         "meta": {
             "install_location": "~\\Documents\\Paradox Interactive\\Stellaris\\mod"
         }
     },
     "Hearts of Iron IV": {
-        "invoke": ["http://96.227.58.11:160/Public/Hearts%20of%20Iron%20IV/invoke.7z", "lmao"],
+        "invoke": ["example.com/Public/Hearts%20of%20Iron%20IV/invoke.7z", "lmao"],
         "meta": {
             "install_location": "~\\Documents\\Paradox Interactive\\Hearts of Iron IV\\mod"
         }
